ca0042753502fec3a2b6a528b9a66168d334e101
GabionStefanie <152822901+GabionStefanie@users.noreply.github.com>
2024-05-27 15:56:18 +0800

    Update changePSword.php

--- Forgot Password/changePSword.php
+++ Forgot Password/changePSword.php
@@ -19,18 +19,24 @@
                 <h1 class="title">FORGOT PASSWORD</h1>
                 <hr>
                 <!-- <p> -->
+                <div style="display: flex; flex-direction: column; align-items:baseline">
                     <label>New Password</label><br>
                     <input type="password" name="password" id="password" required>
 
-                <!-- </p> -->
-                <input id="checkbox-pass1" type="checkbox" onclick="togglePasswordVisibility('password')" />
-                <label for="checkbox-pass1"> Show Password</label><br>
-                <!-- <p> -->
+                    <!-- </p> -->
+                    <div>
+                        <input id="checkbox-pass1" type="checkbox" onclick="togglePasswordVisibility('password')" />
+                        <label for="checkbox-pass1">Show Password</label><br>
+                    </div>
+                    <!-- <p> -->
                     <label>Confirm Password</label><br>
                     <input type="password" name="confirm_password" id="confirm_password" required>
-                <!-- </p> -->
-                <input id="checkbox-pass2" type="checkbox" onclick="togglePasswordVisibility('confirm_password')" />
-                <label for="checkbox-pass2"> Show Password</label>
+                    <!-- </p> -->
+                    <div>
+                        <input id="checkbox-pass2" type="checkbox" onclick="togglePasswordVisibility('confirm_password')" />
+                        <label for="checkbox-pass2"> Show Password</label>
+                    </div>
+                </div>
                 <div id="message" style="color:red;"></div>
                 <div class="button-container">
                     <input type="submit" value="SUBMIT">
