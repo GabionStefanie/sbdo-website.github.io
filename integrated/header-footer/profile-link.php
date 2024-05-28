26f556fec5ef098e9b7d0e857c1da19a9ab547bc
GabionStefanie <152822901+GabionStefanie@users.noreply.github.com>
2024-05-28 12:26:54 +0800

    Update profile-link.php

--- integrated/header-footer/profile-link.php
+++ integrated/header-footer/profile-link.php
@@ -2,19 +2,24 @@
 $conn = new mysqli("localhost", "root", "", "sbdoDatabase");
 if (isset($_COOKIE["User_ID"])) {
     $UserID = $_COOKIE['User_ID'];
-    $sql = "SELECT Account_Type 
+    $sql = "SELECT Account_Type, ProfilePicture 
             FROM account
             WHERE User_ID = '$UserID' ";
     $result = mysqli_fetch_assoc(mysqli_query($conn, $sql));
 
+    $picture = $result['ProfilePicture'];
+
 
     $Account_Type = $result['Account_Type'];
+    // $profilePicture = "<img src='$picture' alt='Profile Picture'>";
+
 } else {
     $Account_Type = null;
 }
 $conn->close();
 
 $profileframe = "";
+
 if ($Account_Type === 'Patient') {
     $profileframe = '<a href="../My Account/account-dashboard.php" class="nav__link"><div class="profile-frame"></div></a>';
 } elseif ($Account_Type === 'Admin') {
