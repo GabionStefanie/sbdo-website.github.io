57b1576b18b2b2a85a326e4974a9e5132a37517a
ExalaJohn <151590435+ExalaJohn@users.noreply.github.com>
2024-05-31 05:44:19 +0800

    Update admin-appointments[done]-page.php

--- integrated/admin-reports/admin-appointments[done]-page.php
+++ integrated/admin-reports/admin-appointments[done]-page.php
@@ -48,33 +48,44 @@
     $dbname = "sbdoDatabase";
 
     $mysqli = new mysqli($servername, $username, $password, $dbname);
-    $sql = "SELECT p.name, p.phone, p.email, p.gender, sc.scheduleDateTime, s.service_name, cc.chief_complaint, cc.details, cc.dentalpain_status, cc.dental_trauma, cc.bleeding_tissues, mh.medical_conditions, mh.diabetes, mh.heart_conditions, mh.hypertension, mh.maintenance, mh.systolic_pressure, mh.diastolic_pressure, GROUP_CONCAT(DISTINCT ma.medicine_name SEPARATOR ', ') AS medicine_name, GROUP_CONCAT(DISTINCT hs.symptom_name SEPARATOR ', ') AS symptom_name, pll.pain_value, hd.covidvaccine_status, pd.referenceno, pd.amount, pd.image_filename, a.appointment_id
-            FROM appointment a
-            JOIN patient p ON a.patient_id = p.patient_id
-            JOIN service s ON a.service_id = s.service_id
-            JOIN chief_complaint cc ON a.appointment_id = cc.chief_complaint_id
-            JOIN medical_history mh ON a.appointment_id = mh.medical_history_id
-            JOIN health_declaration hd ON a.appointment_id = hd.health_declaration_id
-            JOIN payment pd ON a.appointment_id = pd.paymentDetails_ID
-            JOIN schedule sc ON a.appointment_id = sc.schedule_id
-            JOIN patient_medical_allergens pma ON a.appointment_id = pma.medical_history_id
-            JOIN medical_allergens_list ma ON pma.med_allergen_id = ma.med_allergen_id
-            JOIN patient_health_symptoms phs ON a.appointment_id = phs.health_declaration_id
-            JOIN health_symptoms hs ON phs.healthsymptoms_id = hs.healthsymptoms_id
-			JOIN patient_pain_level ppl ON a.appointment_id = ppl.chief_complaint_id
-            JOIN pain_level_list pll ON ppl.pain_level_id = pll.pain_level_id 
-            WHERE p.patient_status = 'Done'
-            GROUP BY a.appointment_id
-            ORDER BY sc.scheduleDateTime";
+    $sql = "SELECT sc.scheduleDate, sc.scheduleTime, pd.paymentdetails_id, ac.name, ac.phone, ac.gender, ac.email, s.service_name, cc.chief_complaint, cc.details, cc.dentalpain_status, cc.dental_trauma, cc.bleeding_tissues, mh.medical_conditions, mh.diabetes, mh.heart_conditions, mh.hypertension, mh.maintenance, mh.systolic_pressure, mh.diastolic_pressure, GROUP_CONCAT(DISTINCT ma.medicine_name SEPARATOR ', ') AS medicine_name, GROUP_CONCAT(DISTINCT hs.symptom_name SEPARATOR ', ') AS symptom_name, pll.pain_value, hd.covidvaccine_status, pd.referenceno, pd.amount, pd.image_filename, a.appointment_id
+    FROM account ac
+    JOIN patient p ON ac.user_id = p.user_id
+    JOIN appointment a ON p.patient_id = a.appointment_id
+    JOIN service s ON a.service_id = s.service_id
+    JOIN chief_complaint cc ON a.appointment_id = cc.chief_complaint_id
+    JOIN medical_history mh ON a.appointment_id = mh.medical_history_id
+    JOIN health_declaration hd ON a.appointment_id = hd.health_declaration_id
+    JOIN patient_medical_allergens pma ON a.appointment_id = pma.medical_history_id
+    JOIN medical_allergens_list ma ON pma.med_allergen_id = ma.med_allergen_id
+    JOIN patient_health_symptoms phs ON a.appointment_id = phs.health_declaration_id
+    JOIN health_symptoms hs ON phs.healthsymptoms_id = hs.healthsymptoms_id
+    JOIN patient_pain_level ppl ON a.appointment_id = ppl.chief_complaint_id
+    JOIN pain_level_list pll ON ppl.pain_level_id = pll.pain_level_id
+    LEFT JOIN payment pd ON a.appointment_id = pd.paymentDetails_ID
+    LEFT JOIN schedule sc ON a.schedule_id = sc.schedule_id 
+    WHERE p.patient_status = 'Done'
+    GROUP BY p.patient_id
+    ORDER BY sc.scheduleDate";
+    
     $result = $mysqli->query($sql);
     while($row = $result->fetch_assoc()) {
         $id = uniqid();
+
+        $scheduleDate = $row['scheduleDate'];
+        $scheduleTime = $row['scheduleTime'];
+
+        // Combine and format date and time
+        $datetime = new DateTime("$scheduleDate $scheduleTime");
+        $formattedDatetime = $datetime->format('m/d/Y h:i A');
         ?>
                         <div class="dropdown">
                             <button id="buttonDropdown-<?php echo $id; ?>" class="buttonDropdown"><?php echo $row["name"]; ?></button>
                             <div id="dropdownData-<?php echo $id; ?>-event" class="dropdown-content">
                                 <table>
-                                    <tr><th>Appointment Date:</th><td><?php echo $row["scheduleDateTime"]; ?></td></tr>
+                                    <tr><th>Appointment ID:</th><td><?php echo $row["appointment_id"]; ?></td></tr>
+                                    <tr><th>Payment ID:</th><td><?php echo $row["paymentdetails_id"]; ?></td></tr>
+                                    <tr><th>Appointment Schedule:</th><td><?php echo $formattedDatetime; ?></td></tr>
                                     <tr><th>Phone:</th><td><?php echo $row["phone"]; ?></td></tr>
                                     <tr><th>Email:</th><td><?php echo $row["email"]; ?></td></tr>
                                     <tr><th>Gender:</th><td><?php echo $row["gender"]; ?></td></tr>
@@ -95,9 +106,17 @@
 									<tr><th>Medical Allergen:</th><td><?php echo $row["medicine_name"]; ?></td></tr>
 									<tr><th>Health Symptoms:</th><td><?php echo $row["symptom_name"]; ?></td></tr>
 									<tr><th>Pain Level:</th><td><?php echo $row["pain_value"]; ?></td></tr>
-                                    <tr><th>Payment Details:</th><td><?php echo $row["referenceno"]; ?></td></tr>
+                                    <tr><th>Reference No.:</th><td><?php echo $row["referenceno"]; ?></td></tr>
 									<tr><th>Amount:</th><td><?php echo $row["amount"]; ?></td></tr>
-									<tr><th>Proof of Payment</th><td> <a href="images/<?php echo $row['image_filename']; ?>" target="_blank"><img src="images/<?php echo $row['image_filename']; ?>" alt="Proof of Payment" style="max-width: 50%; max-height: auto; cursor: pointer;" id="image-<?php echo $row['image_filename']; ?>"></a></td></tr>
+									<tr><th>Proof of Payment</th><td>  <a href="../appointment-form/backend/images/<?php echo $row['image_filename']; ?>" target="_blank">
+            <img src="../appointment-form/backend/images/<?php echo $row['image_filename']; ?>" alt="Proof of Payment" style="max-width: 50%; max-height: auto; cursor: pointer;" id="image-<?php echo $row['image_filename']; ?>"></a></td></tr>
+                                        <tr>
+                                        <td colspan="2">
+                                            <form action="" method="post">
+                                                <input type="hidden" name="appointment_id" value="<?php echo $row["appointment_id"]; ?>">
+                                                <input type="submit" value="Mark as Done" class="mark-done-button">
+                                            </form>
+                                        </td>
                                 </table>
                             </div>
                         </div>
