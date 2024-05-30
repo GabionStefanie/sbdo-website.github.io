d00a0fb7e55a789a649fe48f7c67eaafad54ec3e
ExalaJohn <151590435+ExalaJohn@users.noreply.github.com>
2024-05-31 05:48:59 +0800

    Update appstep1.php

--- integrated/appointment-form/appstep1.php
+++ integrated/appointment-form/appstep1.php
@@ -15,27 +15,26 @@
 
         <?php include "../header-footer/header-footer.css"; ?>
     </style>
-
 </head>
 
 <body>
-<div id="loginModal" class="modal">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <span class="close">&times;</span>
-                    <h2>Login to Proceed</h2>
-                </div>
-                <div class="modal-body">
-                    <p>It seems that you are not yet logged in.</p>
-                    <p>Please log in to be able to book an appointment.</p>
-
-                    <a href="../Login/login.php"><button>LOG IN</button></a>
-                </div>
+    <div id="loginModal" class="modal">
+        <div class="modal-content">
+            <div class="modal-header">
+                <span class="close">&times;</span>
+                <h2>Login to Proceed</h2>
+            </div>
+            <div class="modal-body">
+                <p>It seems that you are not yet logged in.</p>
+                <p>Please log in to be able to book an appointment.</p>
+                <a href="../Login/login.php"><button>LOG IN</button></a>
             </div>
         </div>
+    </div>
+
     <div class="wrapper">
         <?php include "../header-footer/header.php"; ?>
-        
+
         <div class="title">
             <div class="film"></div>
             <img class="contactImg" src="images/contactImg.png" alt="featured image: inside of the dental office" />
@@ -46,46 +45,12 @@
             <p>Appointment Form</p>
         </div>
 
-        <div class="APPOINTMENT-FORM-container" id = "appointment">
+        <div class="APPOINTMENT-FORM-container" id="appointment">
             <form action="backend/process-appstep1.php" method="post" id="appstep1" novalidate style="text-align: left;">
                 <div class="input-group">
                     <div class="patient-information">Patient Information</div>
 
-                    <?php if (isset($_GET['error'])){ ?>
-
-                    <div class="error-message" style = "color: red" > Error: <?php echo $_GET['error']?> </div>
-
-               <?php } ?>
-
-
-                    <div class="flex-group">
-                        <label>Name</label>
-                        <input type="text" id="fname" name="fname" placeholder="First Name" required>
-                        <input type="text" id="lname" name="lname" placeholder="Last Name" required>
-                    </div>
-                </div>
-
-                <div class="input-group flex-group">
-                    <label for="pnum">Phone Number</label>
-                    <input type="text" id="pnum" name="pnum" placeholder="Phone Number" required>
-                </div>
-
-                <div class="input-group flex-group">
-                    <label for="email">Email Address</label>
-                    <input type="email" id="email" name="email" placeholder="Email Address" required>
-                </div>
-
-                <div class="input-group">
-                    <label for="gender">Gender</label>
-                    <select id="gender" name="gender" required>
-                        <option value="" disabled selected>Select an option</option>
-                        <option value="Male">Male</option>
-                        <option value="Female">Female</option>
-                        <option value="Other">Other</option>
-                    </select>
-                </div>
-
-                <div class="input-group">
+                    <div class="input-group">
                     <label for="apptype">Type of Appointment</label>
                     <select id="apptype" name="apptype" required>
                         <option value="" disabled selected>Select an option</option>
@@ -103,41 +68,113 @@
                     </select>
                 </div>
 
-                <div class="input-group" style="display: flex;">
-                    <label for="date" font-family: "Josefin Sans" , "sans-serif" ;>Date: <br></label>
-                    <div style="display: flex;">
-                        <div>
-                            <select id="date" name="date" style="font-family: 'Inter', sans-serif; padding: 0.5rem; border-radius: 5px; border: 1px solid #ccc; margin-left: 2rem;">
-                                <option value="" disabled selected>Select schedule</option>
-                                <!-- PHP code to fetch data from database -->
-                                <?php
-                                $conn = new mysqli("localhost", "root", "", "sbdoDatabase");
-                                $query = "SELECT scheduleDateTime FROM schedule WHERE status = 'available'";
-                                $result = mysqli_query($conn, $query);
-                                while ($row = mysqli_fetch_assoc($result)) {    
-                                    $scheduleDate = $row["scheduleDateTime"];
-                                    echo "<option value='$scheduleDate'>$scheduleDate</option>";
-                                }
-                                ?>
-                            </select>
+                    <div class="input-group" style="display: flex;">
+                        <label for="date">Date: <br></label>
+                        <div style="display: flex;">
+                            <div>
+                                <select id="date" name="date" style="font-family: 'Inter', sans-serif; padding: 0.5rem; border-radius: 5px; border: 1px solid #ccc; margin-left: 2rem;">
+                                    <option value="" disabled selected>Select schedule</option>
+                                    <?php
+                                    $conn = new mysqli("localhost", "root", "", "sbdoDatabase");
+                                    $query = "SELECT DISTINCT scheduleDate FROM schedule WHERE status = 'available'";
+                                    $result = mysqli_query($conn, $query);
+
+                                    $dates = array();
+                                    while ($row = mysqli_fetch_assoc($result)) {
+                                        $dates[] = $row["scheduleDate"];
+                                    }
+
+                                    // Remove duplicate dates
+                                    $uniqueDates = array_unique($dates);
+
+                                    foreach ($uniqueDates as $date) {
+                                        echo "<option value='$date'>$date</option>";
+                                    }
+                                    ?>
+                                </select>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="button-group">
-                    <button type="reset">RESET INFO</button>
-                    <?php if (isset($_COOKIE['User_ID'])) {
+
+                    <div class="input-group">
+                        <label for="time">Time: <br></label>
+                        <div style="display: flex;">
+                            <div>
+                                <select id="time" name="time">
+                                    <option value="" disabled selected>Select time</option>
+                                    <!-- Time options will be populated here -->
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+
+                    <script>
+                        document.getElementById('date').addEventListener('change', function() {
+                            var selectedDate = this.value;
+                            var xhr = new XMLHttpRequest();
+                            xhr.open('GET', 'get_times.php?date=' + selectedDate, true);
+                            xhr.onreadystatechange = function() {
+                                if (xhr.readyState === 4 && xhr.status === 200) {
+                                    var times = JSON.parse(xhr.responseText);
+                                    var timeSelect = document.getElementById('time');
+                                    timeSelect.innerHTML = '<option value="" disabled selected>Select time</option>'; // Reset time options
+
+                                    times.forEach(function(time) {
+                                        // Convert time to AM/PM format
+                                        var timeParts = time.split(':');
+                                        var hours = parseInt(timeParts[0], 10);
+                                        var minutes = timeParts[1];
+                                        var ampm = hours >= 12 ? 'PM' : 'AM';
+                                        hours = hours % 12;
+                                        hours = hours ? hours : 12; // Handle midnight (00:00) as 12 AM
+                                        var formattedTime = hours + ':' + minutes + ' ' + ampm;
+
+                                        var option = document.createElement('option');
+                                        option.value = time;
+                                        option.text = formattedTime;
+                                        timeSelect.appendChild(option);
+                                    });
+
+                                }
+                            };
+                            xhr.send();
+                        });
+
+
+                    </script>
+
+                    <?php
+                    if (isset($_GET['date'])) {
+                        $selectedDate = $_GET['date'];
+                        $conn = new mysqli("localhost", "root", "", "sbdoDatabase");
+                        $query = "SELECT DISTINCT scheduleTime FROM schedule WHERE scheduleDate = '$selectedDate' AND status = 'Available'";
+                        $result = mysqli_query($conn, $query);
+
+                        $times = array();
+                        while ($row = mysqli_fetch_assoc($result)) {
+                            $times[] = $row["scheduleTime"];
+                        }
+                        echo '<script>var times = ' . json_encode($times) . ';</script>';
+                    }
                     ?>
+
+
+                    <div class="button-group">
+                        <button type="reset">RESET INFO</button>
+                        <?php if (isset($_COOKIE['User_ID'])) { ?>
                         <input type="submit" value="NEXT" id="submit-comment">
-                    <?php } else { ?>
+                        <?php } else { ?>
                         <input type="button" id="login-button" value="SUBMIT">
-                    <?php } ?>
+                        <?php } ?>
+                    </div>
                 </div>
             </form>
         </div>
+
         <div class="map">
             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3861.775095013898!2d121.00472617494823!3d14.5548499859261!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397c96d86c62c73%3A0xe913d861b4f9bb63!2sSulit%20%26%20Bagasan%20Dental%20Office!5e0!3m2!1sen!2sph!4v1701731077552!5m2!1sen!2sph" width="600" height="450" style="border: 0" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-
         </div>
+
         <div class="film2">
             <div class="content2">
                 <p>WHERE TO FIND US</p>
@@ -148,6 +185,7 @@
                 E-mail: sulitandbagasan@gmail.com
             </div>
         </div>
+
         <div class="film3">
             <div class="content3">
                 <p>WHEN ARE WE AVAILABLE</p>
@@ -163,6 +201,7 @@
 
         <?php include "../header-footer/footer.php"; ?>
     </div>
+
     <script>
         var modal = document.getElementById("loginModal");
         var btn = document.getElementById("login-button");
