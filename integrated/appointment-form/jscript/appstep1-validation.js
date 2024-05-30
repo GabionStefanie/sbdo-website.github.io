075487d905a06e28dbb9a98d320e36f9ca3e5a7c
ExalaJohn <151590435+ExalaJohn@users.noreply.github.com>
2024-05-31 05:56:13 +0800

    Update appstep1-validation.js

--- integrated/appointment-form/jscript/appstep1-validation.js
+++ integrated/appointment-form/jscript/appstep1-validation.js
@@ -2,46 +2,22 @@ const validation = new JustValidate("#appstep1");
 
 
 validation
-    .addField("#fname", [
-        {
-            rule: "required"
-        }
-    ])
-    .addField("#lname", [
-        {
-            rule: "required"
-        }
-    ])
-  
-    .addField("#pnum", [
-        {
-            rule: "required"
-        }
-    ])
-    .addField("#email", [
-        {
-            rule: "required"
-        },
-        {
-            rule: "email"
-        },
-    ])
-    .addField("#gender", [
-        {
-            rule: "required"
-        }
-    ])
- 
+
     .addField("#apptype", [
         {
             rule: "required"
         }
     ])
+
     .addField("#date", [
         {
             rule: "required"
         }
-        
+    ])
+    .addField("#time", [
+        {
+            rule: "required"
+        }
     ])
    
     .onSuccess((event) => {
